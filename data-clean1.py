eb590bbe47a55663efe78c6ce08c6a8ab2710dd1
ShaitZhou <1967871544@qq.com>
2023-03-10 10:24:51 +0800

    将字符串全部转换成其对应的编码

--- data-clean1.py
+++ data-clean1.py
@@ -1,18 +1,24 @@
 import pandas as pd
-
+import pickle
+from sklearn.preprocessing import LabelEncoder
 # 读取 CSV 文件并选取需要处理的列范围
-df = pd.read_csv("C:\\Users\\19678\\Desktop\\data\\.ipynb_checkpoints\\train-checkpoint.csv", low_memory=False)
-cols = df.columns[362:372]
+data = pd.read_csv("C:\\Users\\19678\\Desktop\\data\\.ipynb_checkpoints\\train-checkpoint.csv", low_memory=False)
 
-# 定义将大写字母转换为小写字母的函数
-def convert_lower_case(x):
-    if isinstance(x, str):
-        return x.lower()
-    else:
-        return x
 
-# 使用 applymap() 方法将指定列范围内的数据中的大写字母转换为小写字母
-df[cols] = df[cols].applymap(convert_lower_case)
+# 创建LabelEncoder对象
+le = LabelEncoder()
+
+# 对特征列进行编码转换
+# 选择object类型的列（包括字符串类型）
+obj_cols = data.select_dtypes(include=['object']).columns
+
+# 将选择的列应用LabelEncoder转换
+for col in obj_cols:
+    data[col] = le.fit_transform(data[col])
+
+
+# 打印转换后的数据
+print(data.head())
 
 # 将处理后的数据写入新的 CSV 文件中
-df.to_csv("C:\\Users\\19678\\Desktop\\data\\.ipynb_checkpoints\\test.csv", index=False)
+data.to_csv("C:\\Users\\19678\\Desktop\\data\\.ipynb_checkpoints\\test.csv", index=False)
