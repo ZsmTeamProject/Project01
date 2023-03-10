e58a3e6f74ca726d4c1670c1e9f3e7afd482138e
ShaitZhou <1967871544@qq.com>
2023-03-10 10:38:17 +0800

    将字符串全部转换成其对应的编码

--- data-clean2.py
+++ data-clean2.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 # 读取 CSV 文件并选取需要处理的列
 df = pd.read_csv("C:\\Users\\19678\\Desktop\\data\\.ipynb_checkpoints\\test.csv", low_memory=False)
-col = "birth_date"
 
+columns_to_process = df.columns[13]
 # 使用 str 属性和字符串的切片方法删除对应列的月份和日期
-df[col] = df[col].astype(str).apply(lambda x: x[:13])
+df[columns_to_process] = df[columns_to_process].astype(str).apply(lambda x: x[:4])
 
 # 将处理后的数据写入新的 CSV 文件中
 df.to_csv("C:\\Users\\19678\\Desktop\\data\\.ipynb_checkpoints\\test2.csv", index=False)
